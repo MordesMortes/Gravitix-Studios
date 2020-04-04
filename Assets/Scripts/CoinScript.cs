8e446ce0519b297f37358e147c46f8ce9ef53ac0
mordes <mordes@gmail.com>
2020-04-03 21:42:35 -0600

    edits to CoinScript

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinScript : MonoBehaviour
     
 {
-    [Range(1f,10f)]
-    public int CoinWeight; 
-    void Start()
+    [Range(1f,30f)]
+    public int CoinWeight;//weight of this coin for the second puzzle
+    public bool IsFake;//is this a real or fake coin for the first puzzle
+    Text CoinValue;//coin value will match the weight
+    private void Awake()
     {
-        
+        CoinValue = GetComponent<Text>();
+    }
+    public void EnScale(float scale)
+    {
+        gameObject.transform.localScale = gameObject.transform.localScale * scale;
+    }
+    public void Reveal()
+    {
+        CoinValue.text = CoinWeight.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
