5861cfc1f515534662f696fdb390daff24b4d451
mordes <mordes@gmail.com>
2020-04-03 19:56:56 -0600

    re adding scripts

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+    
+{
+    [Range(1f,10f)]
+    public int CoinWeight; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
