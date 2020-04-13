296c3dfd5d78f25b34b508ebbc72ca7c66785bc9
mordes <mordes@gmail.com>
2020-04-13 13:25:22 -0600

    added scales and objects to main scene

--- Assets/Scripts/RightScaleScript.cs
+++ Assets/Scripts/RightScaleScript.cs
@@ -6,27 +6,27 @@ using UnityEngine.UI;
 public class RightScaleScript : MonoBehaviour
 {
     public GameObject ScaleDisplay;//the object that displays the scales value.
-    Text Display;// The value that displays on the cube
+    Text Display;// The value that displays on the scale remove if not using(1)
     [HideInInspector]
     public int CoinWeight;
-    MeshRenderer MyRend;//meshrenderer to change colours for testing
+    MeshRenderer MyRend;//meshrenderer to change colours for testing remove if not using (2)
     private void Start()
     {
-        MyRend = gameObject.GetComponent<MeshRenderer>();
-        Display = gameObject.GetComponent<Text>();
+        MyRend = gameObject.GetComponent<MeshRenderer>();//(2)
+        Display = gameObject.GetComponent<Text>();//(1)
         CoinWeight = 0;
     }
     private void OnCollisionEnter(Collision collision)
     {      
        
             CoinWeight = CoinWeight + collision.gameObject.GetComponent<CoinScript>().CoinWeight;
-            Display.text = CoinWeight.ToString();
+            Display.text = CoinWeight.ToString();//(1)
             ScaleDisplay.gameObject.GetComponent<ScaleDisplayScript>().Weigh();
 
        
         if (collision.gameObject.GetComponent<CoinScript>().IsFake)
         {
-            MyRend.material.color = Random.ColorHSV();
+            MyRend.material.SetColor("_BaseColor", Random.ColorHSV());// remove entire if statement if not using (2)
         }
     }
     private void OnCollisionExit(Collision collision)
@@ -34,13 +34,14 @@ public class RightScaleScript : MonoBehaviour
         if (CoinWeight != 0)
         {
             CoinWeight = CoinWeight - collision.gameObject.GetComponent<CoinScript>().CoinWeight;
-            Display.text = CoinWeight.ToString();
+            Display.text = CoinWeight.ToString();//(1)
             ScaleDisplay.gameObject.GetComponent<ScaleDisplayScript>().Weigh();
 
         }
         if (collision.gameObject.GetComponent<CoinScript>().IsFake)
         {
-            MyRend.material.color = Color.grey;
+            MyRend.material.SetColor("_BaseColor", Color.gray);//remove entire if statement if not using (2)
+            
         }
     }
 
