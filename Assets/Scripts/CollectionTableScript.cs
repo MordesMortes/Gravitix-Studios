d3960fa500b6c430d29686b73543b13bd83315b1
mordes <mordes@gmail.com>
2020-04-15 14:16:26 -0600

    added logic to the light puzzle

---
+++ Assets/Scripts/CollectionTableScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionTableScript : MonoBehaviour
+{
+    int Blockcount;
+    public GameObject BalanceScales;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            Blockcount++;
+        }
+        if (Blockcount == 5)
+        {
+            BalanceScales.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            Blockcount--;
+        }
+    }
+}
