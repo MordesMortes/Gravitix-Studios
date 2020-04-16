e58b7cfa92757e6d525e8de1fa2197584bb23f8f
mordes <mordes@gmail.com>
2020-04-15 21:57:52 -0600

    changed block script to take shaders as input

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,10 +6,14 @@ public class Block : MonoBehaviour
 {
 
     private MeshRenderer myRend;
-
+    
     private void Start()
     {
         myRend = GetComponent<MeshRenderer>();
         myRend.material.SetColor("_BaseColor",Random.ColorHSV());
+    }
+    public void Shade(string Shade)
+    {
+        myRend.material.shader = Shader.Find(Shade);
     }
 }
