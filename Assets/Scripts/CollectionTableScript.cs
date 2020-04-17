8212ad7de451f9920440c8cc1330fde76b3eb3f6
mordes <mordes@gmail.com>
2020-04-17 11:00:20 -0600

    added hologram effect to when objects with coinscript collide with collectiontable object added switching off and on of realtime to match the hologram effect.

--- Assets/Scripts/CollectionTableScript.cs
+++ Assets/Scripts/CollectionTableScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
 public class CollectionTableScript : MonoBehaviour
 {
@@ -9,22 +10,30 @@ public class CollectionTableScript : MonoBehaviour
     public GameObject BalanceScales;
     public AudioSource Success;//success sound
     public AudioSource Dissapointment;//failure sound
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Block"))
         {
             Blockcount++;
+            collision.gameObject.GetComponent<RealtimeView>().enabled = false;
+            collision.gameObject.GetComponent<RealtimeTransform>().enabled = false;
+            collision.gameObject.GetComponent<CoinScript>().Shade("Hologram");
         }
         if (Blockcount == 5)
         {
             BalanceScales.SetActive(true);
+
         }
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Block"))
         {
-            Blockcount--;
+            Blockcount--; 
+            collision.gameObject.GetComponent<RealtimeView>().enabled = true;
+            collision.gameObject.GetComponent<RealtimeTransform>().enabled = true;
+            collision.gameObject.GetComponent<CoinScript>().Shade("lit");
         }
     }
 }
